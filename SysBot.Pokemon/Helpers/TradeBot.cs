cf8570d5e043598ff078d59c985015f4d04c13de
Secludedly <GogoplataSteve@Gmail.com>
2026-05-12 22:30:16 -0400

    Update Dependencies
    v8.0.5
    
    Updated custom PKHeX & ALM dependencies.

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v8.0.4";
+        public const string Version = "v8.0.5";
     }
 }
