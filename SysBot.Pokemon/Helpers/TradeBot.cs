456457d999bd3e6db177087cc2025edb202842cd
Secludedly <GogoplataSteve@Gmail.com>
2026-05-27 04:04:50 -0400

    Update PKHeX.Core.dll
    v8.1.3
    
    Updated PKHeX selectively with 4 commits up to 05-27-2026.
    
    - MysteryGift: Gen5+=>pk* respect criteria IVs
    Extract a common IV generator method that has considerations for over-specified and under-specified criteria, rather than deadlocking/returning invalid IV spreads.
    
    - Slot9a: overqwil defer, ignore hasTracker
    https://projectpokemon.org/home/forums/topic/57375-pkhex-new-update-legality-errors-contribution-page/page/36/#findComment-300382
    ZA origin can't travel to adjacent games to explore those evolution methods, so the formarg evo verification fails. Exert more preference for an exact species-match, don't settle for a Qwilfish just cuz it has a tracker.
    
    - WA9: check all IVs if all specified
    
    - swsh: another use of exeggutor static
    https://projectpokemon.org/home/forums/topic/57375-pkhex-new-update-legality-errors-contribution-page/page/36/#findComment-300436
    https://bulbapedia.bulbagarden.net/wiki/Workout_Sea#Wanderers

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v8.1.2";
+        public const string Version = "v8.1.3";
     }
 }
