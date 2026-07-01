31f6487ebd1705b00462e74e6668329d1b43f3a6
Secludedly <GogoplataSteve@Gmail.com>
2026-07-01 00:22:55 -0400

    Update PKHeX.Core.dll
    v8.2.5
    
    Updated PKHeX selectively up to 07-01-2026.
    
    - Add Champions Ver. 1.1.0 assets
    
    - PA8: fix ExtraBytes unused-offset typos
    0x018 -> 0x108: 0x18 is the MarkingValue offset (an actively-used
    field), so listing it among the "unused" ExtraBytes exposed it in the
    byte editor and let edits there clobber the low marking byte (markings
    0-3). 0x108 is the byte actually intended in the 0x100-0x10F run.
    0xE9 -> 0xEB: the "0xDE-0xEB unused" range listed 0xE9 twice and omitted
    0xEB.
    
    - HOME: handle BD/SP -1 same as disassembly
    BD/SP => HOME sanitizes -1=>0 and 0=>658. HOME is therefore "clean" so the other side-data do not need to know about BD/SP goofiness.
    
    - Add support for game specific fonts
    Future pull requests may be added with other font files to add game-specific font files (such as Gen1/2 having JPN/KOR/INT in the pixelated style like SciresM/Rhydon)
    For now, the Control provides via EntityContext. If we need to provide different granularity, maybe another enum would be OK. Possibly add that as a property to IStringConverter.

--- SysBot.Pokemon/Helpers/TradeBot.cs
+++ SysBot.Pokemon/Helpers/TradeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v8.2.4";
+        public const string Version = "v8.2.5";
     }
 }
